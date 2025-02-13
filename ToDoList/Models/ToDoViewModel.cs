6b8f90c5ebc85bc15b7a333db62e72fbad8b3441
DewJSD <jeffreywdew@gmail.com>
2025-02-13 10:50:14 -0600

    Step 3 completed

--- ToDoList/Models/ToDoViewModel.cs
+++ ToDoList/Models/ToDoViewModel.cs
@@ -4,12 +4,15 @@ namespace ToDoList.Models
 {
     public class ToDoViewModel
     {
-        CurrentTask = new ToDo();
+        public ToDoViewModel() 
+        {
+            CurrentTask = new ToDo();
+        }
+        public Filters Filters { get; set; }
+        public List<Status> Statuses { get; set; }
+        public List<Category> Categories { get; set; }
+        public Dictionary<string, string> DueFilters { get; set; }
+        public List<ToDo> Tasks { get; set; }
+        public ToDo CurrentTask { get; set; } //used for Add
     }
-    public Filters Filters {  get; set; }
-    public List<Status> Statuses {  get; set; }
-    public List<Category> Categories { get; set; }
-    public Dictionary<string, string> DueFilters {  get; set; }
-    public List<ToDo> Tasks {  get; set; }
-    public ToDo CurrentTask {  get; set; } //used for Add
 }
